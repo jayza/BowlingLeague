725540d2be748fb486d3908132f57b374c635189
jza <jake@kirkandstanley.se>
2015-06-28 19:26:13 +0200

    Modified scoreboard template and scoreboard loading function.

--- src/Frontend/js/templates.js
+++ src/Frontend/js/templates.js
@@ -1,8 +1,19 @@
+/**
+ * This file contains the templates that are going to be rendered
+ * by Mustache.js.
+ */
+
+// Template directory from root folder.
 var dir = 'src/Frontend/templates/';
 
 function loadScoreboard() {
-  $.get(dir + 'scoreboard.mst', function(template) {
-    var rendered = Mustache.render(template, {test: ''});
-    $('#target').html(rendered);
+  var json;
+  $.get('scoreboard.php', function(data) {
+    json = data;
+    $.get(dir + 'scoreboard.mst', function(template) {
+      console.log(JSON.parse(json));
+      var rendered = Mustache.render(template, {scoreboard: JSON.parse(json)});
+      $('#target').html(rendered);
+    });
   });
 }
